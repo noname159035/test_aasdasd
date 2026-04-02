def4101dce87b50b59ce1709488f1a5cc6bcf9a5
MakSIS <72928539+noname159035@users.noreply.github.com>
2026-04-02 17:08:20 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,7 +1,9 @@
 #include <iostream>
 
-// TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 int main() {
     std::cout << "Привет Git!!!";
-    // TIP See CLion help at <a href="https://www.jetbrains.com/help/clion/">jetbrains.com/help/clion/</a>. Also, you can try interactive lessons for CLion by selecting 'Help | Learn IDE Features' from the main menu.
+}
+
+void F() {
+    std::cout << "Привет Функция";
 }
